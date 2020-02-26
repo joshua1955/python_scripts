f6c3a87807a90a7d7e1e0df412118f8bbefbd7e0
joshua1955 <58981873+joshua1955@users.noreply.github.com>
2020-02-26 10:04:58 +0700

    Update pc.py

--- pc.py
+++ pc.py
@@ -1,3 +1,5 @@
+  GNU nano 2.9.3                                          pc.py                                                     
+
 import random
 chel=0
 min=1
@@ -8,15 +10,19 @@ print("""Инструкция
 2 - больше
 3 - угадал""")
 while pc!=chel:
-	print("Ваше число больше или меньше, чем", pc)
-	choose=int(input(''))
-	if choose==1:
-		max=pc
-		pc=random.randint(min,pc)
-	elif choose==2:
-		min=pc
-		pc=random.randint(pc,max)
-	else:
-		chel=pc
-		print("Ваше число", chel)
-	
+        print("Ваше число больше или меньше, чем", pc)
+        choose=int(input(''))
+        if choose==1:
+                max=pc
+                pc=random.randint(min,pc)
+                while pc==max:
+                         pc=random.randint(min,pc)
+        elif choose==2:
+                min=pc
+                pc=random.randint(pc,max)
+                while pc==min:
+                        pc=random.randint(pc,max)
+        else:
+                chel=pc
+                print("Ваше число", chel)
+
